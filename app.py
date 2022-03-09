02ae82a25085656ded65c9220d2fd575bbf5549a
Denisse Lara <denisse.lara@goluuk.com>
2022-03-09 10:57:56 -0400

    refactor(auth): use Oauth2 Authorization token instead of a manually defined key in the header

--- app.py
+++ app.py
@@ -26,8 +26,9 @@ def token_required(f):
     def decorated(*args, **kwargs):
         token = None
 
-        if "x-access-token" in request.headers:
-            token = request.headers["x-access-token"]
+        if "Authorization" in request.headers:
+            # splitting string 'Bearer <token>'
+            token = request.headers["Authorization"].split(" ")[1]
 
         if not token:
             return jsonify({"message": "Token is missing."}), 401
